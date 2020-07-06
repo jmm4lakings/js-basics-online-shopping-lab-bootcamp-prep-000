31c6a29d3e6131b3f345f7ca354bed7f4f1dc078
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 23:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,5 +57,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.")
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+
+  cart = []
 }
+
