519d748afc6cd5c6d698e0dbf05180619f8d799b
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 23:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,25 +9,16 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) 
+function addToCart(item) {
 var price = Math.floor(Math.random() * 99 + 1);
  var newItem = {[item]: price};
  console.log(`${item} has been added to your cart.`);
  cart.push(newItem);
  return cart;
 }	
+
 function viewCart() {
-  if (cart.length === 0) {
-    console.log ("Your shopping cart is empty.")
-  } else {
-    var items = [];
-    for (var i = 0; i < cart.length; i++) {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item])
-      }
-    }
-    console.log("In your cart, you have " + items.join(", ") + ".");
-  }
+  
 }
 
 function total() {
