99cabf76563131dc7c18e4613d947edfcf738be8
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 23:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,20 +18,37 @@ var price = Math.floor(Math.random() * 99 + 1);
 }	
 
 function viewCart() {
-  
-}
-
-function total() {
-   let t = 0
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
+  if(cart.length == 0) {
+    console.log('Your shopping cart is empty.');
+  } else {
+    var message = 'In your cart, you have ';
+    var item = cart[0];
+    var itemName = Object.keys(item)[0];
+    var itemPrice = item[itemName];
+    message += `${itemName} at $${itemPrice}`;
+    if(cart.length == 2) {
+      var item2 = cart[1];
+      var itemName2 = Object.keys(item2)[0];
+      var itemPrice2 = item2[itemName2];
+      message += ` and ${itemName2} at $${itemPrice2}`;
+    } else if(cart.length >= 3) {
+      for(let i = 1; i < cart.length - 1; i++) {
+        var varItem = cart[i];
+        var varItemName = Object.keys(varItem)[0];
+        var varItemPrice = varItem[varItemName];
+        message += `, ${varItemName} at $${varItemPrice}`;
+      }
+      var lastItem = cart[cart.length - 1];
+      var lastItemName = Object.keys(lastItem)[0];
+      var lastItemPrice = lastItem[lastItemName];
+      message += `, and ${lastItemName} at $${lastItemPrice}`;
     }
+    message += '.';
+    console.log(message);
   }
 
-  return t
-}
+function total() {
+   
 
 
 function removeFromCart(item) {
