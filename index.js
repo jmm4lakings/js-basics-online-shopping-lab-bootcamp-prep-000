8347c383c59286d2a5e79a0c912255463ec708e4
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 23:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0) {
-    console.log ("Your shopping cart is empty")
+    console.log ("Your shopping cart is empty.")
   }
 }
 
