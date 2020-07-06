92aa55dca2e018cd0d650c98cf12b11c68404466
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 23:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,14 @@ function total() {
 
 
 function removeFromCart(item) {
-  
+  let itemInCart = false
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
